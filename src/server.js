6d27c650f7165052807990fb6db9a818e10edcf3
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-22 18:18:21 +0700

    test

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.end('<h1>Hello TTV</h1><hr>')
 })
 
+app.get('/test', (req, res) => {
+  res.status(200).send('test ne')
+})
+
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running at http://${ hostname }:${ port }/`)
