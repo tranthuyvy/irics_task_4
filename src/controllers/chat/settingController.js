fb20b97d4ae00f180602ac34efdc7aec3c999987
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-28 23:45:39 +0700

    allow join link

--- src/controllers/chat/settingController.js
+++ src/controllers/chat/settingController.js
@@ -109,6 +109,60 @@ const AllowSendMsg = (req, res) => {
   }
 }
 
+const AllowReviewMember = (req, res) => {
+  try {
+    const { conversationId, type } = req.params
+
+    const data = dataService.readData()
+    const conversation = data.Conversation.find(conv => conv.id === conversationId)
+
+    if (!conversation) {
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Conversation not found', success: false })
+    }
+
+    if (type === 'on') {
+      updateSettingValue(conversation.conversationSetting, 7, true)
+    } else if (type === 'off') {
+      updateSettingValue(conversation.conversationSetting, 7, false)
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid type', success: false })
+    }
+
+    dataService.writeData(data)
+
+    return res.status(StatusCodes.OK).json({ message: 'Success', success: true, conversation: conversation })
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal Server Error', success: false })
+  }
+}
+
+const AllowJoinLinkInvite = (req, res) => {
+  try {
+    const { conversationId, type } = req.params
+
+    const data = dataService.readData()
+    const conversation = data.Conversation.find(conv => conv.id === conversationId)
+
+    if (!conversation) {
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Conversation not found', success: false })
+    }
+
+    if (type === 'on') {
+      updateSettingValue(conversation.conversationSetting, 9, true)
+    } else if (type === 'off') {
+      updateSettingValue(conversation.conversationSetting, 9, false)
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid type', success: false })
+    }
+
+    dataService.writeData(data)
+
+    return res.status(StatusCodes.OK).json({ message: 'Success', success: true, conversation: conversation })
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal Server Error', success: false })
+  }
+}
+
 const updateSettingValue = (conversationSettings, targetType, newValue) => {
   const settingIndex = conversationSettings.findIndex(setting => setting.type === targetType)
 
@@ -120,4 +174,4 @@ const updateSettingValue = (conversationSettings, targetType, newValue) => {
   }
 }
 
-export default { PinNoteVoteMsg, AllowVote, AllowNote, AllowSendMsg }
+export default { PinNoteVoteMsg, AllowVote, AllowNote, AllowSendMsg, AllowReviewMember, AllowJoinLinkInvite }
--- src/routes/v1/settingConversation.js
+++ src/routes/v1/settingConversation.js
@@ -7,3 +7,5 @@ export const RouterSettingConversation = (router) =>
         .put('/conversations/setting/group/vote/:conversationId/:type', authenticateToken, Setting.AllowVote)
         .put('/conversations/setting/group/note/:conversationId/:type', authenticateToken, Setting.AllowNote)
         .put('/conversations/setting/group/send-msg/:conversationId/:type', authenticateToken, Setting.AllowSendMsg)
+        .put('/conversations/setting/group/review-member/:conversationId/:type', authenticateToken, Setting.AllowReviewMember)
+        .put('/conversations/setting/group/join-link-invite/:conversationId/:type', authenticateToken, Setting.AllowJoinLinkInvite)
