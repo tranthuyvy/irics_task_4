3d4a18ef5a9e3b67e5ee03da6a5573340f924dbc
huytran2609 <20520551@gm.uit.edu.vn>
2024-01-30 23:06:24 +0700

    update answer vote

--- src/controllers/chat/voteController.js
+++ src/controllers/chat/voteController.js
@@ -3,11 +3,11 @@ import {
     findConversationById,
     findVoteByConversationId,
     findVoteById,
+    findVoteByVoteOptionId,
     updateVoteById,
 } from '~/models/chat';
 import { updateConversationById } from '~/models/conversation.model';
 import { findUserByID } from '~/models/user';
-import generateService from '~/services/generateID';
 
 const createNewVote = async (req, res) => {
     try {
@@ -44,10 +44,10 @@ const createNewVote = async (req, res) => {
                         lastLogin: '2024-01 - 13T06: 34: 44.341Z',
                     };
                     // Tạo mảng options với mỗi option chứa id và giá trị
-                    const generateOptionId = await generateService.generateID();
-                    const optionsWithId = options.map((option) => ({
-                        id: generateOptionId,
+                    const optionsWithId = options.map((option, index) => ({
+                        id: `${conversationId}_${index + 1}`,
                         value: option,
+                        voters: [],
                     }));
                     const vote = {
                         conversationId,
@@ -182,31 +182,44 @@ const getListVote = async (req, res) => {
 const answerVote = async (req, res) => {
     try {
         const { userId } = req.user;
-        const { voteId } = req.params;
-        const { answer } = req.body;
+        const { voteOptionId } = req.body;
 
-        // Kiểm tra xem vote có tồn tại không
-        const vote = await findVoteById(voteId);
+        // Kiểm tra voteOptionId có tồn tại không
+        if (!voteOptionId) {
+            return res.status(400).json({ message: 'Vote option ID is required' });
+        }
+
+        // Lấy thông tin voteOptionId từ cơ sở dữ liệu
+        const vote = await findVoteByVoteOptionId(voteOptionId);
+
         if (!vote) {
             return res.status(404).json({ message: 'Vote not found' });
         }
 
-        // Kiểm tra xem cuộc trò chuyện có cho phép thay đổi câu trả lời không
-        const allowChangeAnswers = vote.allowChangeAnswers || false;
-        const createdByUserId = vote.createdByUser?.id;
-        if (!allowChangeAnswers && userId !== createdByUserId) {
-            return res.status(403).json({ message: 'You are not allowed to change your answer for this vote' });
+        // Kiểm tra xem người dùng đã tham gia bình chọn chưa
+        const hasAnswered = vote.options.some(
+            (option) => option.id === voteOptionId && option?.voters?.includes(userId),
+        );
+
+        if (hasAnswered) {
+            return res.status(400).json({ message: 'You have already answered this vote' });
         }
 
-        // Kiểm tra xem câu trả lời có hợp lệ không
-        const isValidAnswer = vote.options.some((option) => option === answer);
-        if (!isValidAnswer) {
-            return res.status(400).json({ message: 'Invalid answer' });
-        }
+        // Thêm userId vào danh sách người đã bình chọn cho option đó
+        const updatedVote = {
+            ...vote,
+            options: vote.options.map((option) =>
+                option.id === voteOptionId
+                    ? {
+                          ...option,
+                          voters: [...option.voters, userId],
+                      }
+                    : option,
+            ),
+        };
 
-        // Cập nhật câu trả lời cho vote
-        const updatedVote = { ...vote, answer };
-        await updateVoteById(voteId, updatedVote);
+        // Cập nhật thông tin vote vào cơ sở dữ liệu
+        await updateVoteById(vote.id, updatedVote);
 
         return res.status(200).json({ message: 'Answer vote success' });
     } catch (error) {
--- src/models/chat.js
+++ src/models/chat.js
@@ -1,77 +1,90 @@
-import generateService from '../services/generateID'
-import dataService from '../services/dataService'
+import generateService from '../services/generateID';
+import dataService from '../services/dataService';
 
 export const createNote = async (note) => {
-    const data = dataService.readData()
-    const generateNoteId = await generateService.generateID()
+    const data = dataService.readData();
+    const generateNoteId = await generateService.generateID();
 
-    const newNote = { id: generateNoteId, ...note}
-    data.notes.push(newNote)
-    dataService.writeData(data)
-}
+    const newNote = { id: generateNoteId, ...note };
+    data.notes.push(newNote);
+    dataService.writeData(data);
+};
 
 export const updateNoteById = async (noteId, updatedNote) => {
-    const data = dataService.readData()
+    const data = dataService.readData();
     // Tìm index của note trong mảng notes với id tương ứng
-    const index = data.notes.findIndex(note => note.id === noteId)
+    const index = data.notes.findIndex((note) => note.id === noteId);
     // Nếu không tìm thấy note, trả về lỗi 404
     if (index === -1) {
         throw new Error('Note not found');
     }
     data.notes[index] = { ...data.notes[index], ...updatedNote };
-    dataService.writeData(data)
-}
+    dataService.writeData(data);
+};
 
 export const findNoteByConversationId = async (conversationId) => {
     const data = dataService.readData();
-    return data.notes.filter(note => note.conversationId === conversationId) || [];
-}
+    return data.notes.filter((note) => note.conversationId === conversationId) || [];
+};
 
 export const findConversationById = async (conversationId) => {
-    const data = dataService.readData()
-    return await data.Conversation.find(conversation => conversation.id === conversationId)
-}
-
+    const data = dataService.readData();
+    return await data.Conversation.find((conversation) => conversation.id === conversationId);
+};
 
 export const createVote = async (vote) => {
-    const data = dataService.readData()
-    const generateVoteId = await generateService.generateID()
+    const data = dataService.readData();
+    const generateVoteId = await generateService.generateID();
 
-    const newVote = { id: generateVoteId, ...vote}
-    data.votes.push(newVote)
-    dataService.writeData(data)
-}
+    const newVote = { id: generateVoteId, ...vote };
+    data.votes.push(newVote);
+    dataService.writeData(data);
+};
 
 export const findVoteById = async (voteId) => {
-    const data = dataService.readData()
-    return await data.votes.find(vote => vote.id === voteId)
-}
+    const data = dataService.readData();
+    return await data.votes.find((vote) => vote.id === voteId);
+};
 
 export const findVoteByConversationId = async (conversationId) => {
     const data = dataService.readData();
-    return data.votes.filter(vote => vote.conversationId === conversationId) || [];
+    return data.votes.filter((vote) => vote.conversationId === conversationId) || [];
+};
+
+export const findVoteByVoteOptionId = async (voteOptionId) => {
+    const data = dataService.readData();
+
+    for (const vote of data.votes) {
+        for (const option of vote.options) {
+            if (option.id === voteOptionId) {
+                return vote;
+            }
+        }
+    }
+
+    return null;
 };
 
 export const updateVoteById = async (voteId, updatedVote) => {
-    const data = dataService.readData()
+    const data = dataService.readData();
     // Tìm index của vote trong mảng votes với id tương ứng
-    const index = data.votes.findIndex(vote => vote.id === voteId)
+    const index = data.votes.findIndex((vote) => vote.id === voteId);
     // Nếu không tìm thấy vote, trả về lỗi 404
     if (index === -1) {
         throw new Error('Vote not found');
     }
     data.votes[index] = { ...data.votes[index], ...updatedVote };
-    dataService.writeData(data)
-}
+    dataService.writeData(data);
+};
 
 export const deleteNoteById = async (noteId) => {
-    const data = dataService.readData()
+    const data = dataService.readData();
     // Tìm index của note trong mảng notes với id tương ứng
-    const index = data.notes.findIndex(note => note.id === noteId)
+    const index = data.notes.findIndex((note) => note.id === noteId);
     // Nếu không tìm thấy note, trả về lỗi 404
     if (index === -1) {
         throw new Error('Note not found');
     }
     data.notes.splice(index, 1);
-    dataService.writeData(data)
-}
+    dataService.writeData(data);
+};
